77c5968e5c87d668da0f87b6a33dfbdd54b7ba55
Suzanne Aitchison <suzanne@suzannes-mbp.home>
2017-05-23 20:56:50 +0100

    basic front page styling completed

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,18 +4,15 @@ const Header = () => {
 
   return (
     <div className="header">
+    
       <div className="header-text">
         <h1>Suzanne Aitchison</h1>
         <h3>Software Developer</h3>
-        <ul className="nav">
-          <li>GitHub</li>
-          <li>LinkedIn</li>
-          <li>Email</li>
-        </ul>
+        
       </div>
-      <div className="header-img">
-        <img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAA3bAAAAJDZhZWRiY2NjLTFkYmMtNDE5Ni04MTQ4LWMwM2FhZGYyMjk4YQ.jpg" />
-      </div>
+     <div className="header-img">
+       <img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAA3bAAAAJDZhZWRiY2NjLTFkYmMtNDE5Ni04MTQ4LWMwM2FhZGYyMjk4YQ.jpg" />
+     </div>
 
     </div>
 
